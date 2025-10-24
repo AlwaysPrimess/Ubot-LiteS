514ce5424134b1a3daed6bfe031fad20edc4cdcb
AlwaysPrimess <sunarsehsri434@gmail.com>
2025-10-24 16:17:57 +0700

    Create __init__.py

---
+++ userbot/modules/__init__.py
@@ -0,0 +1,16 @@
+# userbot/modules/__init__.py
+from .admin import *
+from .adzan import *
+from .alquran import *
+from .anime import *
+from .alkitab import *
+from .animasi import *
+from .afk import *
+from .archive import *
+from .system import *
+from .utility import *
+
+__all__ = [
+    "admin", "adzan", "alquran", "anime", "alkitab",
+    "animasi", "afk", "archive", "system", "utility"
+]
